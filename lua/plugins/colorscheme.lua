848d95aa90de5a8ed426ca9ae15b5000bc29b274
Hritik <hritiksingh9221@gmail.com>
2025-10-06 23:11:45 +0530

    Kanagawa theme added

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,14 +1,26 @@
--- Setting Colorschmes
-
 return {
-  -- add gruvbox
-  { "ellisonleao/gruvbox.nvim" },
+  "vague2k/vague.nvim",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    -- Setup with minimal options
+    require("vague").setup({
+      transparent = false, -- set to `true` if you want no background
+      style = {
+        comments = "italic",
+        strings = "italic",
+        headings = "bold",
+        -- the rest default to "none"
+      },
+      colors = {
+        -- optionally override core colors if needed
+        -- bg = "#18191a",
+        -- fg = "#cdcdcd",
+        -- etc.
+      },
+    })
 
-  -- Configure LazyVim to load gruvbox
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "gruvbox",
-    },
-  },
+    -- Apply the colorscheme
+    vim.cmd("colorscheme vague")
+  end,
 }
